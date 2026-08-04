42274f2675954f7ba2f2ac0f18e830d3d6a259de
ProttoyAmin <md.jahid.bin.amin@gmail.com>
2026-08-04 17:17:28 +0600

    fix api client next

--- clients/web/src/settings/api/client.base.ts
+++ clients/web/src/settings/api/client.base.ts
@@ -1,11 +1,10 @@
-// shared/api/base.client.ts
-import { api, Client } from "./client";
-import type { AxiosInstance } from "axios";
-import type { ApiResponse } from "./types";
+
 import { config } from "../app";
+import { api } from "./client";
+import type { ApiResponse } from "./types";
 
 export abstract class BaseClient<TResult, TCreateDTO, TUpdateDTO = Partial<TCreateDTO>> {
-    protected client: Client = api;
+    protected client: typeof api.v1.client = api.v1.client;
     protected authorized: boolean = false;
     protected url = config.api.v1.baseUrl
 
@@ -14,27 +13,27 @@ export abstract class BaseClient<TResult, TCreateDTO, TUpdateDTO = Partial<TCrea
     ) { }
 
     async getAll(params?: Record<string, unknown>): Promise<TResult[]> {
-        const response = await this.client.v1.get<ApiResponse<TResult[]>>(this.url + this.endpoint, { params });
+        const response = await this.client.get<ApiResponse<TResult[]>>(this.url + this.endpoint, { params });
         return response.data.data as TResult[];
     }
 
     async getById(id: string): Promise<TResult> {
-        const response = await this.client.v1.get<ApiResponse<TResult>>(`${this.url + this.endpoint}/${id}`);
+        const response = await this.client.get<ApiResponse<TResult>>(this.url + `${this.endpoint}/${id}`);
         return response.data.data as TResult;
     }
 
     async create(data: TCreateDTO): Promise<TResult> {
-        const response = await this.client.v1.post<ApiResponse<TResult>>(this.url + this.endpoint, data);
+        const response = await this.client.post<ApiResponse<TResult>>(this.url + this.endpoint, data);
         return response.data.data as TResult;
     }
 
     async update(id: string, data: TUpdateDTO): Promise<TResult> {
-        const response = await this.client.v1.patch<ApiResponse<TResult>>(`${this.url + this.endpoint}/${id}`, data);
+        const response = await this.client.patch<ApiResponse<TResult>>(this.url + `${this.endpoint}/${id}`, data);
         return response.data.data as TResult;
     }
 
     async delete(id: string): Promise<void> {
-        await this.client.v1.delete(`${this.url + this.endpoint}/${id}`);
+        await this.client.delete(`${this.endpoint}/${id}`);
         return Promise.resolve();
     }
 }
